ad23b8705c94897422426da6d8c45f5523cdc306
AshrafReda91 <ashrafreda91@gmail.com>
2026-09-14 23:01:36 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-const CACHE = "protocol-v4";
+const CACHE = "protocol-v5";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener("message", e => {
+  if (e.data && e.data.type === "SKIP_WAITING") self.skipWaiting();
 });
 self.addEventListener("activate", e => {
   e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
