e9e204f91d3b002ac93085599c2c7dc296438455
AshrafReda91 <ashrafreda91@gmail.com>
2026-09-20 18:29:59 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "protocol-v10";
+const CACHE = "protocol-v15";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
