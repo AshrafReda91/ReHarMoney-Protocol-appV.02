e06f00df37ab7583faeb30149f44c07b7ea56d05
AshrafReda91 <ashrafreda91@gmail.com>
2026-09-25 22:36:46 +0300

    Update: in-app Natiga calendar (Coptic/Gregorian/Hijri) in the menu

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "protocol-v20";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
+const CACHE = "protocol-v21";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./coptic-year.jpg"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
