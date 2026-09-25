78815a3806b00d1c8289261e6a6cb48bae2a28a2
AshrafReda91 <ashrafreda91@gmail.com>
2026-09-25 23:14:42 +0300

    Update: menu order and outline icons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "protocol-v26";
+const CACHE = "protocol-v27";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./coptic-year.jpg"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
